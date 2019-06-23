15c561ef258072c9cc1a06c70e7693fae1663df2
Cameron Panlasigui <c.panlasigui@gmail.com>
2019-06-23 13:54:52 -0700

    Add material UI theme wrapper with dark theme

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { ThemeProvider } from '@material-ui/styles';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <ThemeProvider theme={null}>
+        <App />
+    </ThemeProvider>,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
